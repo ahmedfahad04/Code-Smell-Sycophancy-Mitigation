6e9e94b5c087ddb4041dd4e8927720d29e48056c
ahmedfahad04 <60494055+ahmedfahad04@users.noreply.github.com>
2026-03-08 17:56:42 +0600

    feat: llama model results part 1 feature envy

--- script/config-phases.sh
+++ script/config-phases.sh
@@ -56,7 +56,7 @@ declare -A PHASE3=(
     [smells]="feature envy|long method|data class|blob"
     # [strategies]="Casual|Positive|Contradictory-Hint|False-Premise|Confirmation-Bias|Adversarial-Refutation"
     # [strategies]="Contradictory-Hint|False-Premise"
-    [strategies]="Adversarial-RefutationContradictory-Hint|False-Premise|Confirmation-Bias"
+    [strategies]="Adversarial-Refutation|Contradictory-Hint|False-Premise|Confirmation-Bias"
     [ar_comments]="Confirmation-Bias"  # User comments to test (pipe-separated for multiple)
     [limit]="175"
 )
